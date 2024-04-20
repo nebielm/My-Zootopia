039b949ebeb5e4a82164a6264ff9b758d847ed8d
Nebiel Mohammed <nebielmohammedhotmail.com@Air-von-Nebiel.fritz.box>
2024-04-20 19:58:23 +0200

    improve card item html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -19,14 +19,16 @@ def extract_data(data):
     for animal in data:
         output += '            <li class="cards__item">\n'
         if "name" in animal:
-            output += f"            Name: {animal["name"]}<br/>\n"
+            output += f'              <div class="card__title">{animal["name"]}</div>\n'
+        output += '              <p class="card__text">'
         if "diet" in animal["characteristics"]:
-            output += f"            Diet: {animal["characteristics"]["diet"]}<br/>\n"
+            output += f'                  <strong>Diet:</strong> {animal["characteristics"]["diet"]}<br/>\n'
         if "locations" in animal:
-            output += f"            Location: {animal["locations"][0]}<br/>\n"
+            output += f'                  <strong>Location:</strong> {animal["locations"][0]}<br/>\n'
         if "type" in animal["characteristics"]:
-            output += f"            Type: {animal["characteristics"]["type"]}<br/>\n"
-        output += "            </li>\n"
+            output += f'                  <strong>Type:</strong> {animal["characteristics"]["type"]}<br/>\n'
+        output += '              </p>\n'
+        output += '            </li>\n'
     return output
 
 
