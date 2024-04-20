d16d219cab2e539a454f9b15d7384bd64bb45da3
Nebiel Mohammed <nebielmohammedhotmail.com@Air-von-Nebiel.fritz.box>
2024-04-20 20:30:44 +0200

    formating code

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -13,38 +13,40 @@ def read_html(file):
         return fileobj.read()
 
 
-def extract_data(data):
+
+def serialize_animal(animal_obj):
     """ Iterates through JSON data and returns extracted data """
     output = ''
-    for animal in data:
-        output += '            <li class="cards__item">\n'
-        if "name" in animal:
-            output += f'              <div class="card__title">{animal["name"]}</div>\n'
-        output += '              <p class="card__text">'
-        if "diet" in animal["characteristics"]:
-            output += f'                  <strong>Diet:</strong> {animal["characteristics"]["diet"]}<br/>\n'
-        if "locations" in animal:
-            output += f'                  <strong>Location:</strong> {animal["locations"][0]}<br/>\n'
-        if "type" in animal["characteristics"]:
-            output += f'                  <strong>Type:</strong> {animal["characteristics"]["type"]}<br/>\n'
-        output += '              </p>\n'
-        output += '            </li>\n'
+    output += '            <li class="cards__item">\n'
+    if "name" in animal_obj:
+        output += f'              <div class="card__title">{animal_obj["name"]}</div>\n'
+    output += '              <p class="card__text">\n'
+    if "diet" in animal_obj["characteristics"]:
+        output += f'                  <strong>Diet:</strong> {animal_obj["characteristics"]["diet"]}<br/>\n'
+    if "locations" in animal_obj:
+        output += f'                  <strong>Location:</strong> {animal_obj["locations"][0]}<br/>\n'
+    if "type" in animal_obj["characteristics"]:
+        output += f'                  <strong>Type:</strong> {animal_obj["characteristics"]["type"]}<br/>\n'
+    output += '              </p>\n'
+    output += '            </li>\n'
     return output
 
 
 def write_html(html_data, output, file):
     """ Inserts extracted data from JSON file in the html string and overwrites html file"""
-    new_data = html_data.replace("            __REPLACE_ANIMALS_INFO__\n", output)
+    new_html_data = html_data.replace("            __REPLACE_ANIMALS_INFO__\n", output)
     with open(file, "w") as fileobj:
-        fileobj.write(new_data)
+        fileobj.write(new_html_data)
 
 
 def main():
     """ Gets specific data from JSON file and inserts it into html file """
     animals_data = load_data('animals_data.json')
     old_html_data = read_html('animals_template.html')
-    extracted_animals_data = extract_data(animals_data)
-    write_html(old_html_data, extracted_animals_data, 'animals_template.html')
+    output = ''
+    for animal_obj in animals_data:
+        output += serialize_animal(animal_obj)
+    write_html(old_html_data, output, 'animals_template.html')
 
 
 if __name__ == "__main__":
