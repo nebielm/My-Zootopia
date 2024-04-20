1b49167e066cee23d2f22258be4ba65d6d789195
Nebiel Mohammed <nebielmohammedhotmail.com@Air-von-Nebiel.fritz.box>
2024-04-21 01:35:48 +0200

    Final version: added user choice and create new html according to it

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -13,19 +13,61 @@ def read_html(file):
         return fileobj.read()
 
 
+def get_skin_type(animal_data):
+    """ get list of skin types in  Database """
+    skin_type_list = []
+    print("Available skin types:")
+    for animal in animal_data:
+        if "skin_type" in animal["characteristics"]:
+            if animal["characteristics"]["skin_type"] not in skin_type_list:
+                print(animal["characteristics"]["skin_type"])
+                skin_type_list.append(animal["characteristics"]["skin_type"])
+    print("Missing skin type")
+    skin_type_list.append("Missing skin type")
+    return skin_type_list
+
+
+def get_user_choice(skin_type_list):
+    """ Gets user_choice of skin type """
+    while True:
+        user_choice = input("Enter skin type: ")
+        if user_choice.capitalize() in skin_type_list:
+            break
+        else:
+            print("You've entered a wrong choice. Try again.")
+            continue
+    return user_choice
+
+
 def serialize_animal(animal_obj):
     """ Iterates through JSON data and returns extracted data """
     output = '            <li class="cards__item">\n'
     if "name" in animal_obj:
-        output += f'              <div class="card__title">{animal_obj["name"]}</div>\n'
-    output += '              <p class="card__text">\n'
+        output += f'                <div class="card__title">{animal_obj["name"]}</div>\n'
+    output += '                <div class="card__text">\n                    <ul>\n'
     if "diet" in animal_obj["characteristics"]:
-        output += f'                  <strong>Diet:</strong> {animal_obj["characteristics"]["diet"]}<br/>\n'
+        output += (f'                        '
+                   f'<li><strong>Diet:</strong> {animal_obj["characteristics"]["diet"]}</li>\n')
     if "locations" in animal_obj:
-        output += f'                  <strong>Location:</strong> {animal_obj["locations"][0]}<br/>\n'
+        output += (f'                        '
+                   f'<li><strong>Location:</strong> {animal_obj["locations"][0]}</li>\n')
     if "type" in animal_obj["characteristics"]:
-        output += f'                  <strong>Type:</strong> {animal_obj["characteristics"]["type"]}<br/>\n'
-    output += '              </p>\n            </li>\n'
+        output += (f'                        '
+                   f'<li><strong>Type:</strong> {animal_obj["characteristics"]["type"]}</li>\n')
+    output += '                    </ul>\n                </div>\n            </li>\n'
+    return output
+
+
+def writing_new_string(animals_data, user_choice):
+    """ Writing the string according to user-choice """
+    output = ''
+    for animal_obj in animals_data:
+        if user_choice.capitalize() == "Missing skin type":
+            if "skin_type" not in animal_obj["characteristics"]:
+                output += serialize_animal(animal_obj)
+        elif "skin_type" in animal_obj["characteristics"]:
+            if user_choice.capitalize() == animal_obj["characteristics"]["skin_type"]:
+                output += serialize_animal(animal_obj)
     return output
 
 
@@ -40,9 +82,9 @@ def main():
     """ Gets specific data from JSON file and inserts it into html file """
     animals_data = load_data('animals_data.json')
     old_html_data = read_html('animals_template.html')
-    output = ''
-    for animal_obj in animals_data:
-        output += serialize_animal(animal_obj)
+    skin_types = get_skin_type(animals_data)
+    user_choice = get_user_choice(skin_types)
+    output = writing_new_string(animals_data, user_choice)
     write_html(old_html_data, output, 'animals_template.html')
 
 
